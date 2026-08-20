d51dda81c0d8b6475a236a5a40996a32219dd907
Rithul Basil Mathews <rithul123basil@gmail.com>
2026-08-20 08:50:41 +0530

    Use one-shot decoder restart signal

--- pc/include/local_camera_receiver/named_pipe_sink.hpp
+++ pc/include/local_camera_receiver/named_pipe_sink.hpp
@@ -28,8 +28,7 @@ private:
     std::wstring pipe_name_;
     BoundedPacketQueue queue_;
     std::atomic<bool> running_{false};
-    std::atomic<bool> connected_{false};
-    std::atomic<bool> client_seen_{false};
+    std::atomic<bool> restart_transport_{false};
     std::thread worker_;
     std::atomic<void *> pipe_{nullptr};
 };
